8a93d6cd02ecdc18b936adcc6509d13516e66143
Simone Fortunato <frtsmn94@gmail.com>
2022-07-04 15:54:01 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const kMTratta = parseInt(prompt('Inserisci KM Tratta'));
 const Età = parseInt(prompt('Inserisci età'));
 
 if (Età >= 65)  {
-    const costoBiglietto =parseInt (kMTratta * 0.21) - 45%
-    alert ('il costo del biglietto è'+ costoBiglietto + '€');
+    const costoBiglietto = kMTratta * 0.21 - 45%
+    alert (`il costo del biglietto è di ${costoBiglietto}€`);
 } else if (Età <=18) {
-    const costoBiglietto = parseInt (kMTratta * 0.21) - 20%
-    alert ('il costo del biglietto è'+ costoBiglietto + '€');
+    const costoBiglietto = kMTratta * 0.21 - 20%
+    alert (`il costo del biglietto è di ${costoBiglietto}€`);
 } else {
-    const costoBiglietto = parseInt (kMTratta * 0.21);
-    alert ('il costo del biglietto è'+ costoBiglietto + '€');
+    const costoBiglietto =  kMTratta * 0.21;
+    alert (`il costo del biglietto è di ${costoBiglietto} €`);
 } 
