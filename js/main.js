cef24853e5c06cbfade6bcadd71034d1062b8061
Simone Fortunato <frtsmn94@gmail.com>
2022-07-04 16:40:19 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const kMTratta = parseInt(prompt('Inserisci KM Tratta'));
 const Età = parseInt(prompt('Inserisci età'));
 
+
 if (Età >= 65)  {
-    const costoBiglietto = kMTratta * 0.21 - 45%
-    alert (`il costo del biglietto è di ${costoBiglietto}€`);
+    const costoBiglietto = (kMTratta * 0.21) - ((kMTratta * 0.21) * 0.45);
+    const rounded = Math.round((costoBiglietto + Number.EPSILON) * 100) / 100;
+    alert (`il costo del biglietto è di ${rounded}€`);
 } else if (Età <=18) {
-    const costoBiglietto = kMTratta * 0.21 - 20%
-    alert (`il costo del biglietto è di ${costoBiglietto}€`);
+    const costoBiglietto = (kMTratta * 0.21) - ((kMTratta * 0.21) * 0.20);
+    const rounded = Math.round((costoBiglietto + Number.EPSILON) * 100) / 100;
+    alert (`il costo del biglietto è di ${rounded}€`);
 } else {
     const costoBiglietto =  kMTratta * 0.21;
-    alert (`il costo del biglietto è di ${costoBiglietto} €`);
+    const rounded = Math.round((costoBiglietto + Number.EPSILON) * 100) / 100;
+    alert (`il costo del biglietto è di ${rounded} €`);
 } 
